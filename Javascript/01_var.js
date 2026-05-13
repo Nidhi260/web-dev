ffb30df1edd798d818736b7409a13ed13b7496bb
Nidhi260 <singh1230nidhi@gmail.com>
2026-05-13 19:15:26 +0530

    Learned about variables and datatype in js

---
+++ Javascript/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 12230
+let accountEmail = "nidhi26singh08@gmail.com"
+var accountPassword = "9595"
+accountCity = "Karnal"
+// accountId = 2
+
+accountEmail = "singh1230nidhi@gmail.com"
+accountPassword = "9813"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+/*
+var is not used nowadays because of its functional level and block level scope
+we can define a variable without its datatype like let const
+if we define a variable and do not assign its value then on running it shows undefined for that variable 
+*/
